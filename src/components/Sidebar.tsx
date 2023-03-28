04e1fb464d77c6906fbad539902ee6ef231948e0
unknown <giri@10000>
2023-03-28 22:02:51 +0300

    optimized

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { BiHomeAlt2 } from "react-icons/bi";
 import { BsFillPeopleFill } from "react-icons/bs";
 import { SiSteamdeck } from "react-icons/si";
+import { Link } from 'react-router-dom'
 
 function Sidebar() {
   return (
     <div className='w-full h-screen px-20 py-10 bg-gray-100' >
       <div className='flex flex-col gap-y-4 text-zinc-700 ' >
-        <div className='flex space-x-2 ' > <BiHomeAlt2 /> <h1>Home</h1>  </div>
-        <div className='flex space-x-2 ' > <BsFillPeopleFill /> <h1>Members</h1> </div>
+        <Link to='/' className='flex space-x-2 ' > <BiHomeAlt2 /> <h1>Home</h1>  </Link>
+        <Link to='/product' className='flex space-x-2 ' > <BsFillPeopleFill /> <h1>Product</h1> </Link>
         <div className='flex space-x-2 ' > <SiSteamdeck /> <h1>Originization</h1></div>
       </div>
     </div>
